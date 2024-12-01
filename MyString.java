2537de823f50421960bb42f1a4e17e957f0cbf00
Avi <avigrad7@gmail.com>
2024-12-01 20:24:00 +0200

    Finished MyString.java

--- MyString.java
+++ MyString.java
@@ -18,13 +18,35 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        //Better would be return str.toLowerCase()
+        String output = "";
+        for (int i = 0; i < str.length(); i++) {
+            char currentChar = str.charAt(i);
+            if (currentChar >= 'A' && currentChar <= 'Z') {
+                currentChar = (char)(currentChar + 32);
+            }
+            output += currentChar;
+        }
+        return output;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        //Better would be return str1.contains(str2);
+        for (int i = 0; i < str1.length(); i++) {
+            if (str2.charAt(0) == str1.charAt(i)) {
+
+                //increments index until it the characters no longer match or it goes out of scope
+                int index = 1;
+                while (index < str2.length() && index + i < str1.length() && str2.charAt(index) == str1.charAt(index + i)) {
+                    index++;
+                }
+                //If index is the same as the length of the second string then it means it reached the end and matched
+                if (index == str2.length()) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
