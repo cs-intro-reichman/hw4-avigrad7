aac8e958081e57d3116ed1c0f90f01c3a1061c9a
Avi <avigrad7@gmail.com>
2024-12-01 18:52:04 +0200

    Finished Primes.java

--- Primes.java
+++ Primes.java
@@ -1,5 +1,58 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        //I assume you want me to do it this way but the next way is better and essentially the same
+
+        // int topNumber = Integer.parseInt(args[0]);
+        // boolean[] allValues = new boolean[topNumber + 1];
+        // for (int i = 0; i < allValues.length; i++) {
+        //     allValues[i] = true;
+        // }
+        // allValues[0] = false;
+        // allValues[1] = false;
+
+        // for (int i = 3; i < allValues.length; i++) {
+        //     allValues[i] = i % 2 != 0;
+        // }
+        // for (int i = 3; i < allValues.length; i++) {
+        //     if (allValues[i]) {
+        //         for (int u = i + 1; u < allValues.length; u++) {
+        //             if (allValues[u]) {
+        //                 allValues[u] = u % i != 0;
+        //             }
+        //         }
+        //     }
+        // }
+
+
+        int topNumber = Integer.parseInt(args[0]);
+
+        //Initializing
+        boolean[] allValues = new boolean[topNumber + 1];
+        allValues[0] = false;
+        allValues[1] = false;
+        for (int i = 2; i < allValues.length; i++) {
+            allValues[i] = true;
+        }
+
+
+        for (int i = 0; i < allValues.length; i++) {
+            if (allValues[i]) {
+                for (int u = i + 1; u < allValues.length; u++) {
+                    if (allValues[u]) {
+                        allValues[u] = u % i != 0;
+                    }
+                }
+            }
+        }
+
+        //Printing
+        int howMany = 0;
+        for (int i = 0; i <  allValues.length; i++) {
+            if(allValues[i]) {
+                howMany++;
+                System.out.println(i);
+            }
+        }
+        System.out.println("There are " + howMany + " primes between 2 and " + topNumber + " (" + (int)(((double)howMany / topNumber) * 100) + "% are primes)");
     }
 }
